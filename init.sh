fcb5a39d3e4f9ad823d6f74baaccebd11677f154
obertguo <obbertguo@gmail.com>
2025-07-02 14:17:01 -0400

    changes

---
+++ init.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+USERNAME=$1
+
+install_gnome() {
+    apt-get -y update && apt-get -y upgrade
+    apt-get install gdm3 gnome-shell
+    enable gui login
+    systemctl enable gdm && systemctl set-default graphical.target
+}
+
+install_core_packages() {
+    apt-get -y update && apt-get -y upgrade
+    apt-get install sudo git gnome-terminal gnome-text-editor
+}
+
+add_to_sudoers() {
+    usermod -aG sudo "$USERNAME"
+    if [ $? -ne 0 ]
+    then 
+        echo "Failed to add ${USERNAME} to sudoers"
+    else 
+        echo "Successfully added ${USERNAME} to sudoers"
+    fi
+}
+
+run_init() {
+    if [ "$(whoami)" != 'root' ]
+    then 
+        echo "This init script needs to be run as the root user. (Enter 'su - ' in the terminal)"
+        exit 1
+    fi
+
+    if [ -z "$USERNAME" ]
+    then 
+        echo "Specify your non-root account's username"
+        exit 1
+    fi
+
+    install_gnome
+    install_core_packages
+    add_to_sudoers
+    echo "Rebooting in 5s for changes to be applied"
+    sleep 5 && reboot
+}
--- pipeline.sh
+++ pipeline.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-# For now, we can either run a single playbook by passing in the playbook 
-# by passing in the playbook name, or fallback to a list of playbooks to run if 
-# a playbook is not specified
+# It is expected that this pipeline script is run at the root of this 
+# repo's folder
+if [ "$(basename $(pwd))" != 'debian-setup' ]
+then
+  echo "Please run the pipeline script in the debian-setup directory"
+  exit 1
+fi
+
+# First time pipeline initialization. Install python3
+# and configure python3 venv to install Ansible
+if [ ! -d ./ansible-venv ]
+then
+  sudo apt-get -y update
+  sudo apt-get -y install git python3 python3-pip python3-venv
+
+  python3 -m venv ansible-venv
+  . ./ansible-venv/bin/activate
+  pip3 install ansible ansible-lint
+fi
+
+# For now, we can either run a single playbook by passing in the playbook name, 
+# or fallback to a list of playbooks to run if a playbook is not specified
 PLAYBOOKS_TO_RUN=$1
 
 if [ -z "$1" ]; then
@@ -35,9 +54,11 @@ run_pipeline() {
   # Load Ansible
   . ./ansible-venv/bin/activate || exit 1
 
-  #  Load user .env into shell session if it exists, and reload bashrc
-  if [ -f ~/.env ]; then . ~/.env; fi
-  . ~/.bashrc
+  # Create a new user .env if it does not exist
+
+  # This .env file is dynamically populated by the playbooks for things like setting user PATH variables
+  if [ ! -f ~/.env ]; then touch ~/.env; fi
+  . ~/.env
 
 
   SUDO_PASSWORD=""
@@ -45,9 +66,8 @@ run_pipeline() {
 
   for PLAYBOOK in ${PLAYBOOKS_TO_RUN[@]}; do
     call_playbook $PLAYBOOK $SUDO_PASSWORD
-    #  Reload user .env into shell session after playbook finishes, and reload bashrc
+    #  Reload user .env into shell session after playbook finishes
     if [ -f ~/.env ]; then . ~/.env; fi
-    . ~/.bashrc
   done
 }
 
